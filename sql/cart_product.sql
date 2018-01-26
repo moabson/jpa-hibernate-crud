829d6e7495d689ade18b2d1125242c923176db9e
André Moabson <amsr@ic.ufal.br>
2018-01-26 18:51:35 -0300

    up

--- sql/cart_product.sql
+++ sql/cart_product.sql
@@ -1,10 +1,8 @@
 create table cart_product (
 
-	id serial primary key not null,
 	cart_id integer not null,
 	product_id integer not null,
 	
-
 	FOREIGN KEY (cart_id) REFERENCES carts (id),
 	FOREIGN KEY (product_id) REFERENCES products (id)
 	
